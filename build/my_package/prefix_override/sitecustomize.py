752b9d58030a480282b7a1f29b5ee11a60997f64
Srinjoy <2140301@sliet.ac.in>
2024-01-07 11:53:24 +0530

    new tut repo made

---
+++ build/my_package/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/srinjoy/ros2tut/install/my_package'
---
+++ build/turtlesim/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/srinjoy/ros2tut/src/ros_tutorials/turtlesim
+# Build directory: /home/srinjoy/ros2tut/build/turtlesim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("turtlesim__py")
